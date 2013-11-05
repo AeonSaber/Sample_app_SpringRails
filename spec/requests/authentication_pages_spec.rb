13dc50ff15fde99716c98d33fda250ab05ddfa34
Joseph Sutton <jpsutton@madisoncollege.edu>
2013-11-04 18:51:48 -0600

    Finish sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,26 @@
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_error_message('Invalid') }
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { valid_signin(user) }
+
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
+  end
+end
